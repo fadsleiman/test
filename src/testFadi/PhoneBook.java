ea5aa3fca02a537ec85defdd193d0441aaea0439
fadsleiman <fadi.sleiman@hotmail.com>
2016-07-29 16:33:41 -0400

    formating

--- src/testFadi/PhoneBook.java
+++ src/testFadi/PhoneBook.java
@@ -1,34 +1,40 @@
 package testFadi;
+
 //test git
 public class PhoneBook {
 	int id;
 	String personName;
 	String phoneNumber;
-	
-	public PhoneBook(int i, String personName, String phoneNumber) {  
-		  super();  
-		  this.id = i;  
-		  this.personName = personName;
-		  this.phoneNumber = phoneNumber;
-		 }  
-		 public int getId() {  
-		  return id;  
-		 }  
-		 public void setId(int id) {  
-		  this.id = id;  
-		 }  
-		 public String getPersonName() {  
-		  return personName;  
-		 }  
-		 public void setpersonName(String personName) {  
-		  this.personName = personName;  
-		 }   
-		 public String getPhoneNumber()
-		 {
-			return phoneNumber; 
-		 }
-		 public void setphoneNumber(String phoneNumber)
-		 {
-			 this.phoneNumber = phoneNumber;
-		 }
+
+	public PhoneBook(int i, String personName, String phoneNumber) {
+		super();
+		this.id = i;
+		this.personName = personName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setpersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setphoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 }
